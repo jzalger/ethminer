ceb009e1719d130b6f336048bf7e4327dca1c1d4
Jonathan Zalger <jzalger@users.noreply.github.com>
2021-02-20 23:07:21 -0500

    Updated localtime
    Switched localtime to localtime_r

--- libdevcore/Log.cpp
+++ libdevcore/Log.cpp
@@ -29,8 +29,6 @@
 using namespace std;
 using namespace dev;
 
-//⊳⊲◀▶■▣▢□▷◁▧▨▩▲◆◉◈◇◎●◍◌○◼☑☒☎☢☣☰☀♽♥♠✩✭❓✔✓✖✕✘✓✔✅⚒⚡⦸⬌∅⁕«««»»»⚙
-
 // Logging
 unsigned g_logOptions = 0;
 bool g_logNoColor = false;
@@ -60,7 +58,7 @@ LogOutputStreamBase::LogOutputStreamBase(char const* _id)
         {
             time_t rawTime = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
             char buf[24];
-            if (strftime(buf, 24, "%X", localtime(&rawTime)) == 0)
+            if (strftime(buf, 24, "%X", localtime_r(&rawTime)) == 0)
                 buf[0] = '\0';  // empty if case strftime fails
             m_sstr << _id << " " EthViolet << buf << " " EthBlue << std::left << std::setw(8)
                    << getThreadName() << " " EthReset;
